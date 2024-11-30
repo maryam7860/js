89cb8f3ae75114e4d9740aaa98ff086d69059a2c
Maryam Atif <maryamatif800@gmail.com>
2024-11-30 11:18:31 +0500

    ok

---
+++ script.js
@@ -0,0 +1,32 @@
+
+// let element;
+// element=document.querySelector("h2");
+//   element.innerText= element.innerText + " apna clg";
+//  console.log(element.innerText);
+
+
+
+// let element;
+// element=document.querySelectorAll(".box")[0];
+// element.innerText="ghgm";
+// console.log(element.innerText);
+
+// element=document.querySelectorAll(".box")[1];
+// element.innerText="jjhj";
+// console.log(element.innerText);
+
+// element=document.querySelectorAll(".box")[2];
+// element.innerText="sz";
+// console.log(element.innerText);
+
+
+
+
+
+ 
+// h2.innerText=h2.innerText + "apna clg";
+// element=element + apna;
+// document.getElementById("heading").innerText=<h2>hello JS from a</h2>
+// element = document.getElementById("header").innerHTML;
+// console.log(element);
+
